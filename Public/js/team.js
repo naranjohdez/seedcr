efa1bbb34d3c78b24681b8458c021d83c6573dcd
Bruno Sanchez <brunosanchez85@gmail.com>
2016-05-05 22:29:58 -0600

    Added JS for team section - change impact img and added new imgs for the team

--- Public/js/team.js
+++ Public/js/team.js
@@ -1,12 +1,14 @@
 'use strict';
 
 (function ($) {
-    $('#btn-team-read-more').click( function () {
-        $('.hide-text-jm').show(500);
-        $('#btn-team-read-more').hide();
+    $('.btn-team-read-more').click( function () {
+      var userId = $(this).attr('data-id')
+      $('#' + userId + '.hide-text-jm').show(500);
+      $('#btn-team-read-more' + '.' + userId).hide();
     });
-    $('#btn-team-hide').click( function () {
-        $('.hide-text-jm').hide(500);
-        $('#btn-team-read-more').show();
+    $('.btn-team-hide').click( function () {
+      var userId = $(this).attr('data-id')
+      $('#' + userId + '.hide-text-jm').hide(500);
+      $('#btn-team-read-more' + '.' + userId).show();
     });
 }(jQuery));
